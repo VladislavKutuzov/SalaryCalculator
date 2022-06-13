93c7dca2ffcb52ae4c21256e92d24c23a2361566
VladislavKut <vladkutuzov19@gmail.com>
2022-06-13 12:10:07 +0500

    Добавил комментарий в коде и сделал автосохранение историй

--- scripts/add.js
+++ scripts/add.js
@@ -6,45 +6,44 @@ const history = document.querySelector('.history');
 let bool = true;
 let number = 0;
 
-
-
-
-function btnFiftyMinus() {
+function btnFiftyMinus() {  // Расчет -50
     number -= 50;
     text.innerHTML = number;
     console.log('-50');
-    zp();
 
+    // добавление истории расчета
     history.innerHTML = '<div class="history__list"><div class="red-text">' + new Date().toLocaleTimeString() + '</div><div class="history__line red-text">-50</div></div>' + history.innerHTML;
-
+    zp();
 }
 
-function btnFifty() {
+
+function btnFifty() {  // Расчет +50
     number += 50;
     text.innerHTML = number;
-    zp();
     console.log('+50');
 
+    // добавление истории расчета
     history.innerHTML = '<div class="history__list"><div class="green-text">' + new Date().toLocaleTimeString() + '</div><div class="history__line">+50</div></div>' + history.innerHTML;
-
-
-
+    zp();
 }
 
 
-function btnHundred() {
+function btnHundred() {  // Расчет +100
     number += 100;
     text.innerHTML = number;
-    zp();
     console.log('+100');
 
+    // добавление истории расчета
     history.innerHTML = '<div class="history__list"><div class="green-text">' + new Date().toLocaleTimeString() + '</div><div class="history__line">+100</div></div>' + history.innerHTML;
-
+    zp();
 }
 
-function zp() {
+
+function zp() { // Расчет зп от 30% дохода
     sum = text.innerText * 0.3;
     zpDiv.innerText = sum;
+
+    // создаем объекы для автосхранения
     localStorage.setItem('zpDiv', zpDiv.innerText);
     localStorage.setItem('text', text.innerText);
     localStorage.setItem('number', number);
@@ -53,17 +52,13 @@ function zp() {
 }
 
 
-
-
-
-
-
-
-
-/* // Сброс значения если уже новый день
-let data = new Date().setHours(18,47);
-// const newData =  new Date().setHours(18,47)
-console.log(data);
-if (data = newData ) {
-    localStorage.clear();
-} */
+window.onload = () => { // Используем значения для автосохранения элементов страницы
+    let rawText = localStorage.getItem('text');
+    let rawZpDiv = localStorage.getItem('zpDiv');
+    let rawNumber = localStorage.getItem('number');
+    let rawHistory = localStorage.getItem('history');
+    text.innerText = rawText;
+    zpDiv.innerText = rawZpDiv;
+    history.innerHTML = rawHistory;
+    number = Number(rawNumber);
+}
